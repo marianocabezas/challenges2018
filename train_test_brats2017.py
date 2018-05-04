36b8ad00741a688a5f2fb15ce3b137ef55d83d2c
mariano <mcabezas@eia.udg.edu>
2018-05-04 12:59:47 +0200

    - Added testing for roinet.

--- train_test_brats2017.py
+++ train_test_brats2017.py
@@ -361,8 +361,9 @@ def main():
     print('%s[%s] %s<BRATS 2017 pipeline testing>%s' % (c['c'], strftime("%H:%M:%S"), c['y'], c['nc']))
     print('%s[%s] %sCenter computation%s' % (c['c'], strftime("%H:%M:%S"), c['g'], c['nc']))
     # Block center computation
+    overlap = 0 if options['netname'] != 'roinet' else patch_width / 2
     centers = map(
-        lambda names: get_bounding_blocks(load_nii(names[0]).get_data(), patch_width),
+        lambda names: get_bounding_blocks(load_nii(names[0]).get_data(), patch_width, overlap=overlap),
         image_names
     )
 
